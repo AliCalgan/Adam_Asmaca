b9b9f0f4c080391115411c1ca6ceacbe171cd86e
Ali Çalğan <ali.calgan@bilgeadamboost.com>
2023-06-15 00:17:44 +0300

    güncel uygulama

---
+++ Win-Form-Adam_Asmaca/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Win_Form_Adam_Asmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSehir = new System.Windows.Forms.Label();
+            this.pbResim = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSehir
+            // 
+            this.lblSehir.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblSehir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSehir.Location = new System.Drawing.Point(131, 55);
+            this.lblSehir.Name = "lblSehir";
+            this.lblSehir.Size = new System.Drawing.Size(140, 39);
+            this.lblSehir.TabIndex = 0;
+            this.lblSehir.Text = "ŞEHİR";
+            this.lblSehir.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbResim
+            // 
+            this.pbResim.Location = new System.Drawing.Point(426, 32);
+            this.pbResim.Name = "pbResim";
+            this.pbResim.Size = new System.Drawing.Size(348, 416);
+            this.pbResim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbResim.TabIndex = 1;
+            this.pbResim.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbResim);
+            this.Controls.Add(this.lblSehir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form Adam Asmaca";
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSehir;
+        private System.Windows.Forms.PictureBox pbResim;
+    }
+}
+
---
+++ Win-Form-Adam_Asmaca/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Win_Form_Adam_Asmaca.isler;
+
+namespace Win_Form_Adam_Asmaca
+{
+    public partial class Form1 : Form
+    {
+
+        public Bitmap[] Resimler { get; }
+        public Button[] Butonlar { get; } = AdamAsmacaMetotlari.GetirHarfButonlari();
+        public byte Hak { get; set; } = 7;
+        public float Puan { get; set; } = 100;
+        public Form1()
+        {
+            InitializeComponent();
+
+            YeniOyun();
+            
+            this.Controls.AddRange(Butonlar);
+            lblSehir.Text = AdamAsmacaMetotlari.ArayaBoslukEkle(AdamAsmacaMetotlari.EkranMetni);
+
+            Resimler = new Bitmap[]
+            {
+              Properties.Resources._8, Properties.Resources._7,
+              Properties.Resources._6, Properties.Resources._5,
+               Properties.Resources._4, Properties.Resources._3,
+                Properties.Resources._2 
+            };
+        
+  
+        }
+        void YeniOyun()
+        {
+            AdamAsmacaMetotlari.RastgeleSehirSec();
+          
+            for (int i = 0; i < Butonlar.Length; i++)
+            {
+              
+                Butonlar[i].BackColor= Color.White;
+                Butonlar[i].ForeColor = Color.Black;
+                Butonlar[i].Enabled = true;
+                Butonlar[i].Click += HarfeBasildiginda;
+            }
+            lblSehir.Text = AdamAsmacaMetotlari.ArayaBoslukEkle(AdamAsmacaMetotlari.EkranMetni);
+            pbResim.Image = null;
+            Hak = 7;
+
+        }
+        void MesajVer(bool KazanildiMi)
+        {
+            string metin = KazanildiMi ? "Tebrikler Kazandınız " : "Oyunu Kaybettiniz";
+            var cevap = MessageBox.Show($@"{metin}"
+                        + $"\nAranan Şehir : {AdamAsmacaMetotlari.SecilenSehir}\n"
+                        + $"Puan : {Math.Ceiling(Puan)}"
+                        + $"\nDevam Etmek ister misiniz?",
+                        "Oyun Durumu",
+                         MessageBoxButtons.YesNo,
+                         MessageBoxIcon.Question);
+
+            if (cevap == DialogResult.Yes)
+            {
+                Puan += 100;
+                YeniOyun();
+            }
+            else
+            {
+               Application.Exit();
+            }
+        }
+
+        private void HarfeBasildiginda(object sender, EventArgs e)
+        {
+            var buton = ((Button)sender);
+            char basilanHarf = buton.Text[0];
+            string secilenSehir = AdamAsmacaMetotlari.SecilenSehir;
+            string ekranMetni = AdamAsmacaMetotlari.EkranMetni;
+            buton.Enabled = false;
+            if (secilenSehir.Contains(basilanHarf.ToString()))
+            {
+                var dizi = secilenSehir.ToCharArray();
+                var diziEkran = ekranMetni.ToCharArray();
+
+                for (int i = 0; i < dizi.Length; i++)
+                {
+                    if (dizi[i] == basilanHarf)
+                    {
+                        diziEkran[i] = dizi[i];
+                    }
+                }
+                AdamAsmacaMetotlari.EkranMetni = new string(diziEkran);
+                lblSehir.Text = AdamAsmacaMetotlari.ArayaBoslukEkle(new string(diziEkran));
+
+                buton.BackColor = Color.DarkGreen;
+                buton.ForeColor = Color.White;
+                if ((!AdamAsmacaMetotlari.EkranMetni.Contains("_")) && Hak>0 )
+                {
+                    MesajVer(true);
+                }
+
+            }
+            else
+            {
+                buton.BackColor = Color.Red;
+                buton.ForeColor = Color.White;
+                Hak--;
+                pbResim.Image = Resimler[Hak];
+                Puan -= 100f / Resimler.Length;
+
+                if (Hak == 0)
+                    MesajVer(false);                          
+              
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
